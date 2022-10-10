52e5d07b110d3c582918bef4007a158eddddbb30
Mozib <mozib.diu@gmail.com>
2022-10-10 12:39:09 +0600

    API request: Blocked

--- script.js
+++ script.js
@@ -4,3 +4,38 @@ gsap.timeline({ defaults: { ease: "power1.inOut" }, repeat: -1, repeatDelay: 2 }
   .fromTo('.slide_1 .text1', { opacity: 0 }, { opacity: 1, duration: 1 }, ">")
 
 
+var countryone = document.getElementById('countryone')
+var countrytwo = document.getElementById('countrytwo')
+
+var teamone = document.getElementById('teamone')
+var teamtwo = document.getElementById('teamtwo')
+
+getLiveScore();
+// setInterval(function () { getLiveScore(); }, 50000);
+
+//var data;
+
+function getLiveScore() {
+
+  fetch('//ms.purplepatch.online/cricket/api/t20/data.json')
+    .then((response) => response.json())
+    .then((data) => {
+
+      if (data.teamone == false) {
+        teamone.innerText = ''
+      } else {
+        teamone.innerText = data.teamone.split('-')[1];
+      }
+
+      if (data.teamtwo == false) {
+        teamtwo.innerText = ''
+      } else {
+        teamtwo.innerText = data.teamtwo.split('-')[1];
+      }
+
+      countryone.innerText = data.countryone;
+      countrytwo.innerText = data.countrytwo;
+
+    })
+
+}
